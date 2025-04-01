e83cac898d5f2ec4ef0e732e8792b8647e4dfbf0
TheLuis <luisdavidgilcabarcas47@gmail.com>
2025-04-01 18:24:14 -0500

    Implementación de lógica para el contenedor de lista

--- src/App.js
+++ src/App.js
@@ -2,19 +2,20 @@ import './App.css';
 import React, {useState} from 'react';
 import  Header from './components/Header';
 import FormularioTareas from './components/FormularioTareas';
+import ListaTareas from './components/ListaTareas';
 
 const App = () => {
 
   const [tareas, cambiarTareas] = useState(
     [
       {
-        Id: 1,
+        id: 1,
         texto: 'Lavar la ropa',
         completada: true
       },
       {
-        Id: 2,
-        texto: 'Jugar',
+        id: 2,
+        texto: 'Jugar',    
         completada: false
       }
     ]
@@ -26,6 +27,7 @@ const App = () => {
     <div className="contenedor">
       <Header />
       <FormularioTareas tareas={tareas} cambiarTareas={cambiarTareas} />
+      <ListaTareas tareas={tareas} />
     </div>
   );
 }
